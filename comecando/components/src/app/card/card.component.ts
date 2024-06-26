6ed8dc583246cf3813809a119fb1e0f6eaecd8bf
Alex-Trauthman <alextrauthman.full@gmail.com>
2024-06-25 22:28:24 -0300

    optional objects

--- comecando/components/src/app/card/card.component.ts
+++ comecando/components/src/app/card/card.component.ts
@@ -1,20 +1,29 @@
 import { Component } from '@angular/core';
 
+interface IPlano{
+  infos: IInfos;
+}
+interface IInfos{
+  tipo : string;
+  preco: number;
+  prazo: string;
+}
+
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
 export class CardComponent {
-  plano ={
+  plano:any ={
     infos:{
-      tipo: 'Simples',
+      tipo: 'Plano Básico',
       preco: 100,
-      prazo: 'Mês',
+      prazo: 'mês'
     },
     getFullPrice: function(){
-      return'R$'+ this.infos.preco+',00/'+this.infos.prazo;
-    }
+      return'R$'+ this?.infos?.preco +',00/'+this?.infos?.prazo;
+    },
   };
 
 }
